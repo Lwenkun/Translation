ab500316d56bea180b99acd1a4b88b6266d4bfac
lwenkun <liwenkun@meituan.com>
2020-12-01 02:56:24 +0800

    增加免费的谷歌翻译接口

--- src/google.ts
+++ src/google.ts
@@ -1,18 +1,44 @@
 import {v2}  from '@google-cloud/translate';
-import { stringify } from 'querystring';
+import axios, { AxiosResponse } from 'axios';
 import * as vscode from 'vscode';
 
 export default function(query: string) {
-    const translate = new v2.Translate({key : vscode.workspace.getConfiguration().get('translation.google.apiKey')});
-    translate.translate(query, {from: 'en', to: 'zh-CN', }).then((value: [string, any]) => {
-        if (!value || !value[0]) {
-            vscode.window.showErrorMessage('翻译失败，服务器返回的翻译结果为空');
-        } else {
-            vscode.window.showInformationMessage(value[0], '关闭');
-        }
-    }, (reason: any) => {
-        vscode.window.showErrorMessage("翻译失败，" + JSON.stringify(reason));
-    }).catch((reason: any) => {
-        vscode.window.showErrorMessage("翻译失败，" + JSON.stringify(reason));
-    });
+    const key = vscode.workspace.getConfiguration().get<string>('translation.google.apiKey');
+    if (key) {
+        const translate = new v2.Translate({key});
+        translate.translate(query, { from: 'en', to: 'zh-CN', }).then((value: [string, any]) => {
+            if (!value || !value[0]) {
+                vscode.window.showErrorMessage('翻译失败，服务器返回的翻译结果为空');
+            } else {
+                vscode.window.showInformationMessage(value[0], '关闭');
+            }
+        }, (reason: any) => {
+            vscode.window.showErrorMessage("翻译失败，" + JSON.stringify(reason));
+        }).catch((reason: any) => {
+            vscode.window.showErrorMessage("翻译失败，" + JSON.stringify(reason));
+        });
+    } else {
+        const url = `http://translate.google.cn/translate_a/single?client=gtx&dt=t&dj=1&ie=UTF-8&sl=en&tl=zh-CN&q=${query}`;
+        axios.get(url).then((response: AxiosResponse<Result>) => {
+            if (response.status !== 200) {
+                vscode.window.showErrorMessage(`翻译失败，请求失败，code=${response.status}`);
+            } else if (!response.data) {
+                vscode.window.showErrorMessage('翻译失败，服务端返回的数据为空');
+            } else if (!response.data.sentences || response.data.sentences.length === 0) {
+                vscode.window.showErrorMessage('翻译失败，服务器返回的翻译结果为空');
+            } else {
+                vscode.window.showInformationMessage(response.data.sentences[0].trans, '关闭');
+            }
+        }, (reason: any) => {
+            vscode.window.showErrorMessage("翻译失败，" + JSON.stringify(reason));
+        }).catch((reason: any) => {
+            vscode.window.showErrorMessage("翻译失败，" + JSON.stringify(reason));
+        });
+    }
+}
+
+interface Result {
+    sentences: Array<{trans: string, orig: string}>;
+    src: string;
+    spell: {};
 }
